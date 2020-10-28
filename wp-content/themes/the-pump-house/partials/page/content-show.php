6d582aa676d4220ac4a1f91f2b8a86c831f79aa0
MarcusMadMax <marcuszillner@icloud.com>
2020-10-29 11:08:28 +1300

    struggle

--- wp-content/themes/the-pump-house/partials/page/content-show.php
+++ wp-content/themes/the-pump-house/partials/page/content-show.php
@@ -1,28 +1,19 @@
-<?php 
-$terms = get_the_terms(get_the_ID(),'type');
-$classes= "";
+<?php
+    $terms = get_the_terms( get_the_ID(), 'type' );
 
-foreach($terms as $term){
-    $classes.=$term->slug.'';
-}
+    $classes = '';
+    foreach($terms as $term){
+        $classes .= $term->slug . ' ';
+    }
+
 ?>
-<div class="row <?php echo $classes?>">
-    <div class="col-lg-12 d-flex justify-content-center">
-        <ul id="portfolio-flters">
-            <li data-filter="*" class="filter-active"><?php the_title()?></li>
-        </ul>
+Hello World
+<div class="col-12 col-lg-4 col-md-6 portfolio-item <?php echo $classes?>">
+    <img src="<?php the_field('photo')?>" class="img-fluid" alt="">
+    <div class="portfolio-info">
+        <h1>Hello</h1>
+        <h4><?php the_title()?></h4>
+        <p><?php the_content()?></p>
+        <a href="portfolio-details.html" class="details-link" title="More Details">Details</a>
     </div>
-</div>
-
-<div class="row portfolio-container">
-
-    <div class="col-12 col-lg-4 col-md-6 portfolio-item filter-theatre">
-        <img src="<?php the_field('image')?>" class="img-fluid" alt="">
-        <div class="portfolio-info">
-            <h4>A Time to Laugh</h4>
-            <p>26 OCTOBER 2020</p>
-            <a href="portfolio-details.html" class="details-link" title="More Details">Details</a>
-        </div>
-    </div>
-
 </div>
--- wp-content/themes/the-pump-house/whats-on.php
+++ wp-content/themes/the-pump-house/whats-on.php
@@ -1,26 +1,27 @@
 <!-- ======= portfolio Section ======= -->
 <section id="portfolio">
     <div class="container wow fadeInUp">
-
-    <?php
-        while(have_posts()){
-            the_post();
-            get_template_part('partials/content','index');
-        }//end while
-    ?>
+        <?php
+            while(have_posts()){
+                the_post();
+                get_template_part('partials/content','index');
+            }//end while
+        ?>
 
         <div class="row">
-                <div class='mx-auto mb-3'><h3 class="section-title">What's on</h3>
+                <div class='mx-auto mb-3'>
+                    <h3 class="section-title">What's on</h3>
                     <div class="section-title-divider"></div>
                 </div>
             <div class="col-lg-12 d-flex justify-content-center">
                 <ul id="portfolio-flters">
+                    <li data-filter="all" class="filter-active">All</li>
 
                     <?php
-                    $terms = get_terms('type',array('hide_empty'=>false));
-                    foreach($terms as $term){
-                        echo'<li data-filter=".'.$term->slug.'">'.$term->name.'</li>';
-                    }
+                        $terms = get_terms('type',array('hide_empty'=>false));
+                        foreach($terms as $term){
+                            echo'<li data-filter=".'.$term->slug.'">'.$term->name.'</li>';
+                        }
                     ?>
 
                 </ul>
@@ -29,24 +30,22 @@
 
         <div class="row portfolio-container">
 
-            <div class="col-12 col-lg-4 col-md-6 portfolio-item filter-theatre">
-                <?php   
-                    $args = array('post_type' => 'show');
-                    //Get data (services) from database
-                    // The Query
-                    $the_query = new WP_Query( $args );
-                    
-                    // The Loop
-                        while ( $the_query->have_posts() ) {
-                            $the_query->the_post();
-                            get_template_part('partials/page/content','show');
-                        }
+            <?php
+                $args = array('post_type' => 'Shows');
+                // The Query
+                $the_query = new WP_Query( $args );
 
-                    /* Restore original Post Data */
-                    wp_reset_postdata();
+                // The Loop
+                while ( $the_query->have_posts() ) {
+                    $the_query->the_post();
+                    get_template_part('partials/page/content', 'show');
+                }
 
-                ?>
-            </div>
+                /* Restore original Post Data */
+                wp_reset_postdata();
+
+            ?>
+
         </div>
     </div>
 </section><!-- End portfolio Section -->
